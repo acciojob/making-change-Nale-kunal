83ede3be1d3a0f91ce680b63bce5db2bab08d9a6
Nale-kunal <nalekunal343@gmail.com>
2025-10-23 10:07:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const makeChange = (c) => {
-  // your name here
+  // calculate quarters
+  let q = Math.floor(c / 25);
+  c = c % 25;
+
+  // calculate dimes
+  let d = Math.floor(c / 10);
+  c = c % 10;
+
+  // calculate nickels
+  let n = Math.floor(c / 5);
+  c = c % 5;
+
+  // remaining pennies
+  let p = c;
+
+  // return result as an object
+  return { q, d, n, p };
 };
 
 // Do not the change the code below
